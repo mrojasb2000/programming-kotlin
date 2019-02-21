1605f84e0864e238309aa2be408f8686cdf8cb6c
Mauricio Rojas <mrojasb2000@gmail.com>
2019-02-21 16:10:19 -0300

    Refactoring delete script.

--- introduction/delete.kts
+++ introduction/delete.kts
@@ -1,8 +1,12 @@
 import java.io.File
 
-val purgeTime = System.currentTimeMillis() - args[1].toLong() * 24 * 60 * 60 * 1000
+val rootPath = args[0]
+val olderDays = args[1].toLong()
 
-val folders = File(args[0]).listFiles {	file -> file.isFile }
+val purgeTime = System.currentTimeMillis() - olderDays * 24 * 60 * 60 * 1000
+
+val folders = File(rootPath).listFiles { file -> file.isFile }
+
 folders?.filter { file -> file.lastModified() < purgeTime }?.forEach{
 	file -> println("Deleting ${file.absolutePath}")
 	//file.delete()
